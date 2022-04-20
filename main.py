66f2a9b45faeb2e3eea461e3050ff3d339dc163d
Juan Betancourt <juancbetancourt11@gmail.com>
2022-04-20 18:09:28 +0000

    Creating the code for this exercise using the for loop.

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 # 🚨 Don't change the code below 👇
-student_heights = input("Input a list of student heights ").split()
+student_heights = input("Input a list of student heights \n\n").split()
 for n in range(0, len(student_heights)):
   student_heights[n] = int(student_heights[n])
 # 🚨 Don't change the code above 👆
@@ -7,6 +7,16 @@ for n in range(0, len(student_heights)):
 
 #Write your code below this row 👇
 
+total_height = 0
+for height in student_heights:
+  total_height += height
+print(f"\nThe total height for students is {total_height}\n")
 
+number_of_students = 0
+for student in student_heights:
+  number_of_students += 1
+print(f"The total number of students is {number_of_students}\n")
 
+average_height = round(total_height / number_of_students)
 
+print(f"The average height between all students is {average_height}\n")
